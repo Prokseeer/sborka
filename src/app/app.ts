cd90de651c399d50f52fef0a51aeca70043d3cbb
Sergey Alekseev <sergey.alekseev@egamings.com>
2020-05-05 18:14:20 +0300

    SBORKA

---
+++ src/app/app.ts
@@ -0,0 +1,27 @@
+import angular, {IScope} from 'angular';
+
+import '../style/app.scss';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  protected url: string;
+  static $inject = ['$scope'];
+  constructor(protected $scope: IScope) {
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
